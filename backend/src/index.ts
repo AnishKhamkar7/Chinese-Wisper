0bd893f4022a4570136f07022e60ba7b3378d23e
Anish <anishkhamkar7@gmail.com>
2024-11-27 11:13:42 +0530

    [Backend] feat: initialize Socket Connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,13 @@
-import app from "./app";
+import { server } from "./app";
 import dotenv from "dotenv";
+import { io } from "./app";
 
 dotenv.config();
 
-app.listen(process.env.PORT, () => {
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
+});
+
+server.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
