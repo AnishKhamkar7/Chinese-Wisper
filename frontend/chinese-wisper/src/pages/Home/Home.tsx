afe11ad10c947e753deefe86f146eada4c5cc820
Anish <anishkhamkar7@gmail.com>
2024-11-27 11:16:16 +0530

    [Frontend] feat: initialized a socket client connection and username input dialogue box

--- frontend/chinese-wisper/src/pages/Home/Home.tsx
+++ frontend/chinese-wisper/src/pages/Home/Home.tsx
@@ -1,56 +1,121 @@
-import { useQuery } from "@tanstack/react-query";
-import PostCard from "@/components/Posts/PostsCard";
-import postApi from "@/api/postsApi";
-import PostSkeleton from "@/components/Posts/PostsLoading";
+import { io } from "socket.io-client";
+import { AlertCircle } from "lucide-react";
+import { useEffect, useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogFooter,
+} from "@/components/ui/dialog";
+import { Label } from "@radix-ui/react-label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { v4 as uuidv4 } from "uuid";
+import { Socket } from "dgram";
 
-interface PostData {
+const URL = "http://localhost:5000";
+
+interface User {
   id: string;
-  title: string;
-  initialContent: string;
-  currentContent: string | null;
-  userId: string;
-  nextEditTime: string;
-  createdAt: string;
-  updatedAt: string;
+  username: string;
 }
 
-const HomePage = () => {
-  const fetchPosts = async (): Promise<PostData[]> => {
-    const response = await postApi.getAllPosts();
-    return response.data;
+function Home() {
+  const [user, setUser] = useState<User>({
+    id: uuidv4(),
+    username: "",
+  });
+  const [openDialogue, setOpenDialogue] = useState(true);
+  const [error, setError] = useState(false);
+  const [socket, setSocket] = useState<any>();
+
+  const handleSubmit = () => {
+    if (user?.username.trim() === "") {
+      setError(true);
+      return;
+    }
+
+    localStorage.setItem("username", user.username);
+    localStorage.setItem("id", user.id);
+    setOpenDialogue(false);
   };
 
-  const query = useQuery({ queryKey: ["posts"], queryFn: fetchPosts });
+  useEffect(() => {
+    const checkLocalStorage = localStorage.getItem("username");
 
-  if (query.isLoading) {
-    return (
-      <div>
-        <PostSkeleton />
-        <PostSkeleton />
-        <PostSkeleton />
-      </div>
-    );
-  }
+    if (!checkLocalStorage) {
+      console.log("going if");
+      setOpenDialogue(true);
+      const socket = io(URL, {});
+      socket.on("connect", () => {
+        console.log("Socket Connected");
+        setSocket(socket.id);
+      });
 
-  if (query.error) {
-    return <div>Error loading posts: {query.error.message}</div>;
-  }
+      return () => {
+        socket.disconnect();
+      };
+    } else {
+      setOpenDialogue(false);
+      console.log("going else");
+
+      const userObjName = localStorage.getItem("username");
+      const userObjId = localStorage.getItem("id");
+
+      setUser({ id: userObjId!, username: userObjName! });
+    }
+  }, []);
 
   return (
     <div>
-      {query.data?.map((post) => (
-        <PostCard
-          key={post.id}
-          id={post.id}
-          title={post.title}
-          initialContent={post.initialContent}
-          userId={post.userId}
-          createdAt={post.createdAt}
-          clickable={true}
-        />
-      ))}
+      {openDialogue ? (
+        <Dialog open={openDialogue}>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Name</DialogTitle>
+              <DialogDescription>
+                Enter your name or generate your name
+                {error && (
+                  <Alert variant="destructive" className="border-none">
+                    <AlertCircle className="h-4 w-4" />
+                    <AlertTitle>Error</AlertTitle>
+                    <AlertDescription>
+                      Username should not be empty
+                    </AlertDescription>
+                  </Alert>
+                )}
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  UserName
+                </Label>
+                <Input
+                  id="name"
+                  value={user?.username}
+                  onChange={(e) =>
+                    setUser((prev) => ({ ...prev, username: e.target.value }))
+                  }
+                  className="col-span-3 outline-black"
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="button" onClick={handleSubmit}>
+                Save
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      ) : (
+        <h1>Hi {user?.username}</h1>
+      )}
     </div>
   );
-};
+}
 
-export default HomePage;
+export default Home;
