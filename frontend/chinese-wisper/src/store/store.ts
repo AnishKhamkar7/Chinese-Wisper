b1ece8deb8926525df28ad07125a9f55e0e0bfb5
Anish <anishkhamkar7@gmail.com>
2024-11-29 20:23:07 +0530

    [Frontend] feat: added socket and roomProp reducer in store

--- frontend/chinese-wisper/src/store/store.ts
+++ frontend/chinese-wisper/src/store/store.ts
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "@/store/authSlicer";
+import socketSlicer from "./socketSlicer";
+import roomCreateProp from "./roomCreatePropSlicer";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    socketContext: socketSlicer,
+    roomCreateProp: roomCreateProp,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
