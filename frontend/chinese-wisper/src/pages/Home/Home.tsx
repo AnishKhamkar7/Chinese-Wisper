89e7ed7343ec3bdc1bbed54e7525f6011d92d1e3
Anish <anishkhamkar7@gmail.com>
2024-12-08 20:25:56 +0530

    [Frontend] refactor: removed conditional render block for openDialogue

--- frontend/chinese-wisper/src/pages/Home/Home.tsx
+++ frontend/chinese-wisper/src/pages/Home/Home.tsx
@@ -70,47 +70,43 @@ function Home() {
 
   return (
     <div>
-      {openDialogue ? (
-        <Dialog open={openDialogue}>
-          <DialogContent className="sm:max-w-[425px]">
-            <DialogHeader>
-              <DialogTitle>Name</DialogTitle>
-              <DialogDescription>
-                Enter your name or generate your name
-                {error && (
-                  <Alert variant="destructive" className="border-none">
-                    <AlertCircle className="h-4 w-4" />
-                    <AlertTitle>Error</AlertTitle>
-                    <AlertDescription>
-                      Username should not be empty
-                    </AlertDescription>
-                  </Alert>
-                )}
-              </DialogDescription>
-            </DialogHeader>
-            <div className="grid gap-4 py-4">
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="name" className="text-right">
-                  UserName
-                </Label>
-                <Input
-                  id="name"
-                  value={username}
-                  onChange={(e) => setUserName(e.target.value)}
-                  className="col-span-3 outline-black"
-                />
-              </div>
+      <Dialog open={openDialogue}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Name</DialogTitle>
+            <DialogDescription>
+              Enter your name or generate your name
+              {error && (
+                <Alert variant="destructive" className="border-none">
+                  <AlertCircle className="h-4 w-4" />
+                  <AlertTitle>Error</AlertTitle>
+                  <AlertDescription>
+                    Username should not be empty
+                  </AlertDescription>
+                </Alert>
+              )}
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                UserName
+              </Label>
+              <Input
+                id="name"
+                value={username}
+                onChange={(e) => setUserName(e.target.value)}
+                className="col-span-3 outline-black"
+              />
             </div>
-            <DialogFooter>
-              <Button type="button" onClick={handleSubmit}>
-                Save
-              </Button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
-      ) : (
-        <div></div>
-      )}
+          </div>
+          <DialogFooter>
+            <Button type="button" onClick={handleSubmit}>
+              Save
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
