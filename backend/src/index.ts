966daf4a9a9c06f5e715b40d2d1628acec74cfe0
Anish <anishkhamkar7@gmail.com>
2024-12-08 20:24:04 +0530

    [Backend] feat: added roomManager class in the websocket connection to handle createRoom

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,21 @@
 import { server } from "./app";
 import dotenv from "dotenv";
 import { io } from "./app";
+import RoomManager from "./sockets/RoomManager";
 
 dotenv.config();
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
+
+  socket.emit("socketId", socket.id);
+
+  const roomManager = new RoomManager({ io: socket, socketId: socket.id });
+
+  socket.on("RoomDetails", (data) => {
+    const { limit, roomName } = data;
+    roomManager.createRoom({ limit, roomName });
+  });
 });
 
 server.listen(process.env.PORT, () => {
