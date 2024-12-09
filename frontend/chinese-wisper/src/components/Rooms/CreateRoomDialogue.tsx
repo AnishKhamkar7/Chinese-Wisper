d2239ad368d5322e3a47362ee40c8a307caf77d4
Anish <anishkhamkar7@gmail.com>
2024-12-09 22:19:54 +0530

    [Frontend] feat: create room dialogue added with RoomName and Participants limit

--- frontend/chinese-wisper/src/components/Rooms/CreateRoomDialogue.tsx
+++ frontend/chinese-wisper/src/components/Rooms/CreateRoomDialogue.tsx
@@ -9,15 +9,56 @@ import {
 } from "../ui/dialog";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
+import { InputOTP, InputOTPGroup, InputOTPSlot } from "../ui/input-otp";
+import { Alert, AlertTitle } from "../ui/alert";
+import { AlertCircle } from "lucide-react";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { closeCreateRoomDialog } from "@/store/roomCreatePropSlicer";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/store";
 
 function CreateRoomDialogue() {
+  const dispatch = useDispatch();
+  const [limitValue, setLimitValue] = useState("02");
+  const [error, setError] = useState(false);
+
+  const isCreateRoomDialogue = useSelector(
+    (state: RootState) => state.roomCreateProp.isCreateRoomDialogOpen
+  );
+
+  console.log(isCreateRoomDialogue);
+
+  const handleOnChange = (value: string) => {
+    setLimitValue(value);
+    if (Number(value) > 20 || Number(value) < 2) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+  };
+
   return (
     <div>
-      <Dialog open={true}>
+      <Dialog
+        open={isCreateRoomDialogue}
+        onOpenChange={(open) => !open && dispatch(closeCreateRoomDialog())}
+      >
         <DialogContent className="sm:max-w-screen-sm">
           <DialogHeader>
             <DialogTitle>Create A Room</DialogTitle>
-            <DialogDescription>Its Time to Debate!</DialogDescription>
+            <DialogDescription>
+              Its Time to Debate!
+              {error && (
+                <Alert variant="destructive" className="border-none">
+                  <AlertCircle className="h-4 w-4" />
+
+                  <AlertTitle>
+                    Limit should not be greater than 20 or below 2
+                  </AlertTitle>
+                </Alert>
+              )}
+            </DialogDescription>
             <div className="grid gap-2 py-4">
               <div className="grid grid-cols-4 gap-2 items-center">
                 <Label htmlFor="name" className="text-left">
@@ -25,19 +66,24 @@ function CreateRoomDialogue() {
                 </Label>
                 <Input
                   id="name"
-                  value="new"
+                  value=""
                   className="col-span-3 outline-black"
                 />
               </div>
               <div className="grid grid-cols-4 gap-2 items-center">
                 <Label htmlFor="name" className="text-left">
-                  UserName
+                  Limit
                 </Label>
-                <Input
-                  id="name"
-                  value="new"
-                  className="col-span-3 outline-black"
-                />
+                <InputOTP
+                  maxLength={2}
+                  value={limitValue}
+                  onChange={(val) => handleOnChange(val)}
+                >
+                  <InputOTPGroup>
+                    <InputOTPSlot index={0} />
+                    <InputOTPSlot index={1} />
+                  </InputOTPGroup>
+                </InputOTP>
               </div>
             </div>
           </DialogHeader>
