a90110ad80b5820d98af0b18f81ba9c0b7c30001
Anish <anishkhamkar7@gmail.com>
2024-12-11 19:47:19 +0530

    [Frontend] feat: added RoomMembers component to render the members on the Room page

--- frontend/chinese-wisper/src/components/Rooms/RoomMembers.tsx
+++ frontend/chinese-wisper/src/components/Rooms/RoomMembers.tsx
@@ -4,15 +4,7 @@ import { Avatar, AvatarImage } from "../ui/avatar";
 function RoomMembers() {
   return (
     <div>
-      <div
-        className=" w-16 h-16 rounded-full border-2 border-gray-400 hover:border-blue-500 transition-all justify-items-center"
-        //   style={{
-        //     left: `${x}px`,
-        //     top: `${y}px`,
-        //     transform: "translate(-50%, -50%)",
-        //   }}
-        //   title={`${member.name}\n${member.expertise.join(", ")}`}
-      ></div>
+      <div className=" w-16 h-16 rounded-full border-2 border-gray-400 hover:border-blue-500 transition-all justify-items-center"></div>
     </div>
   );
 }
